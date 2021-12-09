6ca6c196ce951a75cee10c0a7274d872c09fc867
Ubuntu <ubuntu@ip-172-31-13-65.ap-south-1.compute.internal>
2021-12-09 12:09:30 +0000

    add file

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cd /var/www/html/laravel_localBAK
+git pull origin circle
+
 ssh -i /home/ubuntu/.ssh/authorized_keys ubuntu@65.2.153.14 "cd /var/www/html/laravel/local && git pull origin circle "
